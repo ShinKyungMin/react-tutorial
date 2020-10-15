c5426a05bcdda6fb790e08e16b124c8d03130c85
A_ALOE <66265554+ShinKyungMin@users.noreply.github.com>
2020-10-15 12:02:16 +0900

    Update AttendanceBook.js

--- function/f-attendance/src/Chapter01/AttendanceBook.js
+++ function/f-attendance/src/Chapter01/AttendanceBook.js
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
-
 const AttendanceBook = () => {
-  const [students] = useState([
-    { id: 1, name: "Mike" },
-    { id: 2, name: "Jane" },
-    { id: 3, name: "Susan" },
-    { id: 4, name: "Steve" },
-    { id: 5, name: "Brad" },
-    { id: 6, name: "Leo" },
-    { id: 7, name: "John" },
-    { id: 8, name: "Sam" },
-    { id: 9, name: "Kate" },
+  const [name1, setName] = useState("");
+  const [students, setStudents] = useState([
+    { id: 1, name: "이순신" },
+    { id: 2, name: "세종대왕" },
+    { id: 3, name: "계백" },
+    { id: 4, name: "궁예" },
+    { id: 5, name: "척준경" },
+    { id: 6, name: "손오공" },
+    { id: 7, name: "삼장" },
+    { id: 8, name: "저팔계" },
+    { id: 9, name: "사오정" },
   ]);
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
 
   return (
-    <ul>
-      {students.map(({ id, name }) => {
-        return <li key={id}>{name}</li>;
-      })}
-    </ul>
+    <form>
+      <ul>
+        {students.map(({ id, name }) => {
+          return <li key={id}>{name}</li>;
+        })}
+        <li>{name1}</li>
+      </ul>
+      <br></br>
+      <label> 이름을 입력하세요 : </label>
+      <input value={name1} onChange={onChangeName} value={name1} />
+    </form>
   );
 };
-
 export default AttendanceBook;
